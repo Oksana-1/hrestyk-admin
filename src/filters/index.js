99c551fc64655ef0feb53f75149749ea654787fd
oksana-1 <4438488@gmail.com>
2020-10-26 19:27:15 +0200

    Fix bug with getMonth

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,7 +1,7 @@
 export const dateToString = (payload) => {
     const date = new Date(payload);
     const day = date.getDate();
-    const month = date.getMonth();
+    const month = date.getMonth() + 1; //starts from 0
     const year = date.getFullYear();
-    return `${day}.${month >=10 ? month: '0' + month}.${year}`;
+    return `${day}.${month >=10 ? month : '0' + month}.${year}`;
 };
