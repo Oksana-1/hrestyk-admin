d5b82bde8e52c2b435ca7b24b0ebf69aa2f4b601
oksana-1 <4438488@gmail.com>
2022-06-05 18:19:52 +0300

    Add error-component

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,63 +1,76 @@
 <template>
   <div>
     <v-container fluid>
-      <v-row justify="center">
+      <api-error v-if="isError"/>
+      <v-row v-else justify="center">
         <v-col cols="12">
-          <BaseCard
-              header-type="avatar"
-              icon-name="mdi-store">
+          <BaseCard header-type="avatar" icon-name="mdi-store">
             <template v-slot:card-content>
-              <v-list-item>
-                <v-row align="center" class="hidden-sm-and-down" no-gutters>
-                  <v-col cols="1" class="text-caption primary--text">Картинка</v-col>
-                  <v-col cols="4" class="text-caption primary--text">Назва товару</v-col>
-                  <v-col cols="2" class="text-caption primary--text">Категорія</v-col>
-                  <v-col cols="1" class="text-caption primary--text">Ціна</v-col>
-                  <v-col cols="1" class="text-caption primary--text">На складі</v-col>
-                  <v-col cols="2" class="text-caption primary--text">Дата</v-col>
-                </v-row>
-              </v-list-item>
-              <v-divider/>
-              <v-list v-if="!busy" avatar="true" class="py-0 mb-5">
-                <product-list-item
+              <template>
+                <v-list-item>
+                  <v-row align="center" class="hidden-sm-and-down" no-gutters>
+                    <v-col cols="1" class="text-caption primary--text"
+                      >Картинка</v-col
+                    >
+                    <v-col cols="4" class="text-caption primary--text"
+                      >Назва товару</v-col
+                    >
+                    <v-col cols="2" class="text-caption primary--text"
+                      >Категорія</v-col
+                    >
+                    <v-col cols="1" class="text-caption primary--text"
+                      >Ціна</v-col
+                    >
+                    <v-col cols="1" class="text-caption primary--text"
+                      >На складі</v-col
+                    >
+                    <v-col cols="2" class="text-caption primary--text"
+                      >Дата</v-col
+                    >
+                  </v-row>
+                </v-list-item>
+                <v-divider />
+                <v-list v-if="!busy" avatar="true" class="py-0 mb-5">
+                  <product-list-item
                     v-for="item in products"
                     :key="item.id"
                     :product="item"
-                    @onProductListDelete="deleteItemFromList($event)"/>
-              </v-list>
-              <v-row class="justify-end">
-                <v-col cols="3" class="d-flex justify-end">
-                  <v-tooltip bottom>
-                    <template v-slot:activator="{ on, attrs }">
-                      <v-btn
+                    @onProductListDelete="deleteItemFromList($event)"
+                  />
+                </v-list>
+                <v-row class="justify-end">
+                  <v-col cols="3" class="d-flex justify-end">
+                    <v-tooltip bottom>
+                      <template v-slot:activator="{ on, attrs }">
+                        <v-btn
                           fab
                           dark
                           color="primary"
                           v-bind="attrs"
                           v-on="on"
                           @click.prevent="addProduct"
-                      >
-                        <v-icon dark>mdi-plus</v-icon>
-                      </v-btn>
-                    </template>
-                    <span>Додати товар</span>
-                  </v-tooltip>
-                </v-col>
-              </v-row>
+                        >
+                          <v-icon dark>mdi-plus</v-icon>
+                        </v-btn>
+                      </template>
+                      <span>Додати товар</span>
+                    </v-tooltip>
+                  </v-col>
+                </v-row>
+              </template>
             </template>
           </BaseCard>
         </v-col>
       </v-row>
     </v-container>
     <confirm-modal
-        :confirmationText="confirmationMessage"
-        v-if="modalToShow === 'confirm'"
-        @confirm="doRemove"
-        @cancel="closeModal"
-        :disabled-button="removing"
+      :confirmationText="confirmationMessage"
+      v-if="modalToShow === 'confirm'"
+      @confirm="doRemove"
+      @cancel="closeModal"
+      :disabled-button="removing"
     />
-    <new-product-modal
-        v-if="modalToShow === 'addProduct'"/>
+    <new-product-modal v-if="modalToShow === 'addProduct'" />
   </div>
 </template>
 
@@ -65,8 +78,9 @@
 import BaseCard from "../components/base/BaseCard";
 import ProductListItem from "./products/ProductListItem";
 import NewProductModal from "./newProduct/NewProductModal";
-import {mapActions, mapGetters, mapMutations} from 'vuex';
+import { mapActions, mapGetters, mapMutations } from "vuex";
 import ConfirmModal from "./modals/ConfirmModal";
+import ApiError from "@/views/errors/ApiError";
 
 export default {
   name: "Products",
@@ -74,39 +88,42 @@ export default {
     return {
       busy: false,
       modalToShow: null,
-      confirmationMessage: 'Видалити цей продукт?',
+      confirmationMessage: "Видалити цей продукт?",
       productIdToDelete: null,
       removing: false,
-    }
+      isError: false,
+    };
   },
   components: {
+    ApiError,
     ProductListItem,
     BaseCard,
     NewProductModal,
-    ConfirmModal
+    ConfirmModal,
   },
   computed: {
-    ...mapGetters(['products', 'dialog']),
+    ...mapGetters(["products", "dialog"]),
   },
   methods: {
-    ...mapActions(['fetchProducts', 'deleteProduct']),
-    ...mapMutations(['SET_DIALOG']),
+    ...mapActions(["fetchProducts", "deleteProduct"]),
+    ...mapMutations(["SET_DIALOG"]),
     addProduct() {
       this.SET_DIALOG(true);
-      this.modalToShow = 'addProduct';
+      this.modalToShow = "addProduct";
     },
     async init() {
       this.busy = true;
       try {
         await this.fetchProducts();
       } catch (e) {
+        this.isError = true;
         console.error(e);
       } finally {
         this.busy = false;
       }
     },
     deleteItemFromList(productId) {
-      this.modalToShow = 'confirm';
+      this.modalToShow = "confirm";
       this.SET_DIALOG(true);
       this.productIdToDelete = productId;
     },
@@ -125,14 +142,12 @@ export default {
       this.SET_DIALOG(false);
       this.modalToShow = null;
       this.productIdToDelete = null;
-    }
+    },
   },
   created() {
     this.init();
-  }
-}
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/views/errors/ApiError.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <v-alert dense dismissible border="left" type="error"
+        >Наш сервер, напевне, заснув. Не будіть його!</v-alert
+      >
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "ApiError",
+};
+</script>
+
+<style scoped></style>
