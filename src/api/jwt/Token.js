09cb15a84e1b5b7664b7620b8d0421193f3e7ee5
oksana-1 <4438488@gmail.com>
2022-08-12 18:26:34 +0300

    Add `expired` to token

--- src/api/jwt/Token.js
+++ src/api/jwt/Token.js
@@ -3,6 +3,7 @@ import { ObjectCreationValidator } from "@/entities/ObjectCreationValidator";
 export class Token {
   constructor(token) {
     this.token = token.token;
+    this.expired = token.expired;
 
     ObjectCreationValidator.validate(this);
   }
