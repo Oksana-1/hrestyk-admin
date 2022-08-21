24e13d3867572c56b2ac1af02bb1772fc2b37a58
oksana-1 <4438488@gmail.com>
2022-08-21 17:08:59 +0300

    Fix mobile on main page

--- src/views/home/orders/MainOrderRow.vue
+++ src/views/home/orders/MainOrderRow.vue
@@ -2,31 +2,24 @@
   <div>
     <v-list-item link :to="`/orders/${order.id}`" class="px-0">
       <v-row align="center" no-gutters class="py-1">
-        <v-col cols="12" md="2">
+        <v-col cols="3" md="2">
           <v-list-item-content>
             <v-list-item-title class="text-caption">
               {{ order.createdAt | dateToString }}
             </v-list-item-title>
           </v-list-item-content>
         </v-col>
-        <v-col cols="12" md="5">
+        <v-col cols="6" md="8">
           <v-list-item-content>
             <v-list-item-title class="text-caption">
               {{ order.userInfo.name }}
             </v-list-item-title>
           </v-list-item-content>
         </v-col>
-        <v-col cols="12" md="2">
+        <v-col cols="3" md="2">
           <v-list-item-content>
             <v-list-item-title class="text-caption">
               {{ order.orderSum }} грн
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-col>
-        <v-col cols="12" md="3">
-          <v-list-item-content>
-            <v-list-item-title class="text-caption">
-              {{ order.orderStatus }}
             </v-list-item-title>
           </v-list-item-content>
         </v-col>
--- src/views/home/products/MainProductRow.vue
+++ src/views/home/products/MainProductRow.vue
@@ -3,13 +3,13 @@
     <v-skeleton-loader type="list-item-avatar" :loading="loading">
       <v-list-item link :to="`/products/${product.id}`" class="px-0">
         <v-row align="center" no-gutters class="py-1">
-          <v-col cols="12" md="2">
+          <v-col cols="2">
             <v-list-item-avatar color="grey lighten-3">
               <v-img v-if="mainImageSrc" :src="mainImageSrc"></v-img>
               <v-icon v-else>mdi-image</v-icon>
             </v-list-item-avatar>
           </v-col>
-          <v-col cols="12" md="10">
+          <v-col cols="10">
             <v-list-item-content>
               <v-list-item-title class="text-caption">{{
                 product.title
