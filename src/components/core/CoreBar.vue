b83879d41d50ae81e0383b6a6015e8d475f60fbd
oksana-1 <4438488@gmail.com>
2022-08-23 13:03:25 +0300

    Add logout

--- src/components/core/CoreBar.vue
+++ src/components/core/CoreBar.vue
@@ -14,14 +14,24 @@
       class="hidden-sm-and-down font-weight-light"
       v-text="$route.name"
     />
+    <v-spacer></v-spacer>
+    <v-btn icon @click="logout">
+      <v-icon>mdi-logout</v-icon>
+    </v-btn>
   </v-app-bar>
 </template>
 
 <script>
 import { mapGetters, mapMutations } from "vuex";
+import JwtManager from "@/api/jwt/JwtManager";
 
 export default {
   name: "CoreBar",
+  data() {
+    return {
+      jwtManager: new JwtManager(),
+    }
+  },
   computed: {
     ...mapGetters("dialogs", ["drawer"]),
   },
@@ -29,6 +39,10 @@ export default {
     ...mapMutations("dialogs", {
       setDrawer: "SET_DRAWER",
     }),
+    logout() {
+      this.jwtManager.deleteUserAccess();
+      this.$router.push("/login");
+    }
   },
 };
 </script>
